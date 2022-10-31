7c301858e078f153b042c5be7caefc16bc8c0a7a
sobunge <samuelobunge@gmail.com>
2022-10-31 14:30:19 +0300

    first commit

---
+++ src/main/java/com/pensasha/cheifManage/CheifManageApplication.java
@@ -0,0 +1,13 @@
+package com.pensasha.cheifManage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheifManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheifManageApplication.class, args);
+	}
+
+}
