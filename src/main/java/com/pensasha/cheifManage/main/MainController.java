632be2d63b45583d4fe67edaaf69689abd03a2be
sobunge <samuelobunge@gmail.com>
2022-11-04 13:46:01 +0300

    updates

--- src/main/java/com/pensasha/cheifManage/main/MainController.java
+++ src/main/java/com/pensasha/cheifManage/main/MainController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.pensasha.cheifManage.user.User;
 import com.pensasha.cheifManage.user.UserService;
