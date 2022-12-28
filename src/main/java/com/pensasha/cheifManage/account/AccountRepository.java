4d66bc5fbb2bef0a82ce71faf318ea8478e64e64
Samuel Odhiambo Obunge <samuelobunge@gmail.com>
2022-12-28 13:19:07 +0300

    updatig my repositories

--- src/main/java/com/pensasha/cheifManage/account/AccountRepository.java
+++ src/main/java/com/pensasha/cheifManage/account/AccountRepository.java
@@ -1,9 +1,7 @@
 package com.pensasha.cheifManage.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface AccountRepository extends JpaRepository<Account, String>{
     
     Account findByUsersIdNumber(int idNumber);
--- src/main/java/com/pensasha/cheifManage/year/YearRepository.java
+++ src/main/java/com/pensasha/cheifManage/year/YearRepository.java
@@ -1,9 +1,7 @@
 package com.pensasha.cheifManage.year;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface YearRepository extends JpaRepository<Year, Short>{
     
 }
